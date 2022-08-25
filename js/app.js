b7b6e55127cd4ff2e82424da964bb6619971e36a
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-25 11:54:33 +0600

    done

--- js/app.js
+++ js/app.js
@@ -7,17 +7,40 @@ const  searchStates = async value=>{
 
    const res = await fetch('./../data/data.json')
    const states = await res.json()
-   console.log(states)
  
- 
-  
-
 //   check match or not 
+let matches = states.filter(state=>{
+    const regx =new RegExp(`^${value}`, 'gi')
+    return  state.name.match(regx)||state.abbr.match(regx)
+   
+})
+if (value.length === 0) {
+    matches = []
+    matchLists.innerHTML = ''
+}
+outputHtml(matches)
+}
 
+// output html
 
-
+const outputHtml = matches =>{
+    if (matches.length >0) {
+        const html = matches.map(match=>`
+        
+        <div class="card card-body mb-1 mx-auto ">
+        <h4>${match.name} (${match.abbr}) <span class="text-primary">
+        ${match.city}
+        </span> </h4>
+        <!-- <small>lat : ${match.lat}</small> -->
+        </div>
+        
+        
+        
+        `).join('')
+        matchLists.innerHTML= html
+    }
 }
 
 
-search.addEventListener('keyup',()=>searchStates(search.value))
+search.addEventListener('input',()=>searchStates(search.value))
 
